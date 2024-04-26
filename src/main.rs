cc6c3e5a7827edab84f67263db70b37ec78467e0
AchroDev <ztblaine@protonmail.com>
2024-04-26 15:31:32 -0500

    Added second function providing an example of the snake_case style in the name

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+// Entry point
 fn main() {
     println!("Hello, world!");
+
+    snake_case();
+}
+
+/*
+*   Rust code uses snake case as the conventional style for function and variable
+*   names, in which all letters are lowercase and underscores separate words.
+*/
+
+fn snake_case() {
+    println!("This is a function using snake case styling for the name!");
 }
