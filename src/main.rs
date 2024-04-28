d21fdd051ac768e440aaa9150c777a028e77ef29
AchroDev <ztblaine@protonmail.com>
2024-04-27 19:56:20 -0500

    added comment at the end of the expression in new_scope_block

--- src/main.rs
+++ src/main.rs
@@ -66,6 +66,12 @@ fn new_scope_block() {
     let y = {
         let x = 3;
         x + 1
+        /*
+        Note that the x + 1 line doesn’t have a semicolon at the end,
+        which is unlike most of the lines you’ve seen so far. Expressions do not
+        include ending semicolons. If you add a semicolon to the end of an expression,
+        you turn it into a statement.
+        */
     };
 
     println!("The value of y is: {y}");
